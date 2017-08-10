6586a7933d2fd9aa10ed1f88c8ddc2994f0a049a
Adith <adithwidyapradipta@gmail.com>
2017-08-11 00:27:02 +0700

    Tampilan game dan udah pecah jadi components

---
+++ client/src/components/SecondRow.vue
@@ -0,0 +1,116 @@
+<template lang="html">
+  <!-- Second row -->
+  <div class="row">
+    <div class="col">
+
+      <div class="row">
+        <div class="col col-btn">
+          <div class="col">
+            <center>
+              <a href="#" class="btn btn-md active" role="button" aria-pressed="true">0</a>
+            </center>
+          </div>
+
+          <div class="col">
+            <center>
+              <a href="#" class="btn btn-md active" role="button" aria-pressed="true">1</a>
+            </center>
+          </div>
+
+          <div class="col">
+            <center>
+              <a href="#" class="btn btn-md active" role="button" aria-pressed="true">2</a>
+            </center>
+          </div>
+
+        </div>
+
+        <div class="col">
+          <center>
+            <img src="/resources/img/skull.png" alt="pumpkin icon">
+          </center>
+          <br>
+          <center>
+            <img src="/resources/img/skull.png" alt="pumpkin icon">
+          </center>
+        </div>
+      </div>
+
+    </div>
+    <div class="col">
+
+      <div class="row">
+        <div class="col">
+          <div class="score-board">
+
+          </div>
+        </div>
+        <div class="col">
+          <div class="score-board">
+
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <center>
+            <a href="#" class="btn btn-md active" role="button" aria-pressed="true">1</a>
+            <a href="#" class="btn btn-md active" role="button" aria-pressed="true">2</a>
+            <a href="#" class="btn btn-md active" role="button" aria-pressed="true">3</a>
+            <a href="#" class="btn btn-md active" role="button" aria-pressed="true">4</a>
+            <a href="#" class="btn btn-md active" role="button" aria-pressed="true">5</a>
+            <a href="#" class="btn btn-md active" role="button" aria-pressed="true">6</a>
+          </center>
+        </div>
+      </div>
+
+    </div>
+    <div class="col">
+
+      <div class="row">
+
+        <div class="col">
+          <center>
+            <img src="/resources/img/ghost.png" alt="pumpkin icon">
+          </center>
+          <br>
+          <center>
+            <img src="/resources/img/ghost.png" alt="pumpkin icon">
+          </center>
+        </div>
+
+        <div class="col col-btn">
+          <div class="col">
+            <center>
+              <a href="#" class="btn btn-md active" role="button" aria-pressed="true">0</a>
+            </center>
+          </div>
+
+          <div class="col">
+            <center>
+              <a href="#" class="btn btn-md active" role="button" aria-pressed="true">1</a>
+            </center>
+          </div>
+
+          <div class="col">
+            <center>
+              <a href="#" class="btn btn-md active" role="button" aria-pressed="true">2</a>
+            </center>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ client/src/components/ThirdRow.vue
@@ -0,0 +1,53 @@
+<template lang="html">
+  <!-- third row -->
+  <div class="row">
+    <div class="col">
+      <!-- blank -->
+    </div>
+    <div class="col">
+
+      <div class="row">
+        <div class="col">
+          <center>
+            <img src="/resources/img/mummyman.png" alt="pumpkin icon">
+          </center>
+        </div>
+        <div class="col">
+          <center>
+            <img src="/resources/img/mummyman.png" alt="pumpkin icon">
+          </center>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <center>
+            <a href="#" class="btn btn-md active" role="button" aria-pressed="true">0</a>
+          </center>
+        </div>
+        <div class="col">
+          <center>
+            <a href="#" class="btn btn-md active" role="button" aria-pressed="true">1</a>
+          </center>
+        </div>
+        <div class="col">
+          <center>
+            <a href="#" class="btn btn-md active" role="button" aria-pressed="true">2</a>
+          </center>
+        </div>
+      </div>
+
+    </div>
+    <div class="col">
+      <!-- blank -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
