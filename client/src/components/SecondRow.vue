2c43c06e4bcf5b612ef23d40aada75fd12f70e0e
budhip <ryo.bunraku@yahoo.com>
2017-08-11 14:21:07 +0700

    component ghost dan skull

--- client/src/components/SecondRow.vue
+++ client/src/components/SecondRow.vue
@@ -8,42 +8,7 @@
     <!-- Ini column pertama -->
     <!-- Kasih aja nama: Skull.vue atau apa aja -->
     <!-- Karena ini column karakter skull -->
-    <div class="col">
-
-      <div class="row">
-        <div class="col col-btn">
-          <div class="col">
-            <center>
-              <a href="#" class="btn btn-md active" role="button" aria-pressed="true">0</a>
-            </center>
-          </div>
-
-          <div class="col">
-            <center>
-              <a href="#" class="btn btn-md active" role="button" aria-pressed="true">1</a>
-            </center>
-          </div>
-
-          <div class="col">
-            <center>
-              <a href="#" class="btn btn-md active" role="button" aria-pressed="true">2</a>
-            </center>
-          </div>
-
-        </div>
-
-        <div class="col">
-          <center>
-            <img src="../assets/img/skull.png" alt="skull icon">
-          </center>
-          <br>
-          <center>
-            <img src="../assets/img/skull.png" alt="skull icon">
-          </center>
-        </div>
-      </div>
-
-    </div>
+    <Skull></Skull>
     <!-- Ini column pertama -->
 
     <!-- Ini column kedua -->
@@ -83,41 +48,7 @@
     <!-- Ini column ketiga -->
     <!-- Kasih aja nama: Ghost.vue atau apa aja -->
     <!-- Karena ini column karakter ghost -->
-    <div class="col">
-
-      <div class="row">
-        <div class="col">
-          <center>
-            <img src="../assets/img/ghost.png" alt="ghost icon">
-          </center>
-          <br>
-          <center>
-            <img src="../assets/img/ghost.png" alt="ghost icon">
-          </center>
-        </div>
-
-        <div class="col col-btn">
-          <div class="col">
-            <center>
-              <a href="#" class="btn btn-md active" role="button" aria-pressed="true">0</a>
-            </center>
-          </div>
-
-          <div class="col">
-            <center>
-              <a href="#" class="btn btn-md active" role="button" aria-pressed="true">1</a>
-            </center>
-          </div>
-
-          <div class="col">
-            <center>
-              <a href="#" class="btn btn-md active" role="button" aria-pressed="true">2</a>
-            </center>
-          </div>
-        </div>
-      </div>
-
-    </div>
+    <Ghost></Ghost>
     <!-- Ini column ketiga -->
 
   <!-- Ini row utama Second row -->
@@ -127,7 +58,13 @@
 </template>
 
 <script>
+import Skull from '@/components/Skull'
+import Ghost from '@/components/Ghost'
 export default {
+  components: {
+    Skull,
+    Ghost
+  }
 }
 </script>
 
---
+++ client/src/components/Skull.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="col">
+
+    <div class="row">
+      <div class="col col-btn">
+        <div class="col">
+          <center>
+            <a href="#" class="btn btn-md active" role="button" aria-pressed="true">0</a>
+          </center>
+        </div>
+
+        <div class="col">
+          <center>
+            <a href="#" class="btn btn-md active" role="button" aria-pressed="true">1</a>
+          </center>
+        </div>
+
+        <div class="col">
+          <center>
+            <a href="#" class="btn btn-md active" role="button" aria-pressed="true">2</a>
+          </center>
+        </div>
+
+      </div>
+
+      <div class="col">
+        <center>
+          <img src="../assets/img/skull.png" alt="skull icon">
+        </center>
+        <br>
+        <center>
+          <img src="../assets/img/skull.png" alt="skull icon">
+        </center>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
