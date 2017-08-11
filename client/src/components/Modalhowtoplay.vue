1388b120373a0496ff8dce7714a2e65ad6308ce2
Adith <adithwidyapradipta@gmail.com>
2017-08-11 14:41:54 +0700

    Modal diganti kata bagian closenya

--- client/src/components/Modalhowtoplay.vue
+++ client/src/components/Modalhowtoplay.vue
@@ -46,7 +46,7 @@
 
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Saya siap!</button>
         </div>
       </div>
     </div>
