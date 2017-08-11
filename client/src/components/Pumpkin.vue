151018245689256cc49a87ce9a4189344220801f
Adith <adithwidyapradipta@gmail.com>
2017-08-11 14:21:12 +0700

    First row dan Third row column playernya dijadiin components

---
+++ client/src/components/Pumpkin.vue
@@ -0,0 +1,48 @@
+<template lang="html">
+  <!-- Ini column kedua -->
+  <!-- Kasih aja nama: Pumpkin.vue atau apa aja -->
+  <!-- Karena ini column karakter pumpkin -->
+  <div class="col">
+
+    <div class="row">
+      <div class="col">
+        <center>
+          <a href="#" class="btn btn-md active" role="button" aria-pressed="true">0</a>
+        </center>
+      </div>
+      <div class="col">
+        <center>
+          <a href="#" class="btn btn-md active" role="button" aria-pressed="true">1</a>
+        </center>
+      </div>
+      <div class="col">
+        <center>
+          <a href="#" class="btn btn-md active" role="button" aria-pressed="true">2</a>
+        </center>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <center>
+          <img src="../assets/img/pumpkin.png" alt="pumpkin icon">
+        </center>
+      </div>
+      <div class="col">
+        <center>
+          <img src="../assets/img/pumpkin.png" alt="pumpkin icon">
+        </center>
+      </div>
+    </div>
+
+  </div>
+  <!-- Ini column kedua -->
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
